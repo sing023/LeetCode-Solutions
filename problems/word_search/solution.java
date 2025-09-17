708f823ca900efaae40a2d75e794f49e3fc103d5
UnZ <62937352+sing023@users.noreply.github.com>
2025-09-17 11:00:22 +0000

    Sync LeetCode submission - Word Search (java)

--- problems/word_search/solution.java
+++ problems/word_search/solution.java
@@ -1,36 +1,34 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int cols= board[0].length;
-
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<cols; j++) {
-                if (board[i][j] == word.charAt(0) && search(board, i, j, word, 0)) {
-                    return true;
+        for(int i=0; i<board.length; i++) {
+            for (int j=0; j<board[0].length; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    if (backtrack(i,j,0,board,word)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
-
-    private boolean search(char[][] board, int i, int j, String word, int index) {
-        if (index == word.length()) {
-            return true;
-        }
-
-        if ( i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] != word.charAt(index)) {
+    
+    public boolean backtrack(int r, int c,int wordIndex,char[][] board, String word) {
+        if (r<0 || r>board.length-1 || c<0 || c>board[0].length-1 || board[r][c] != word.charAt(wordIndex)) {
             return false;
         }
+        if (wordIndex == word.length()-1 ) {
+            return true;
+        }
+        // Mark current cell as visited
+        char temp = board[r][c];
+        board[r][c] = '#';  // Use a placeholder to mark visited
 
-        char temp = board[i][j];
-        board[i][j] = '-';
+        // Recurse in 4 directions
+        boolean found = backtrack(r + 1, c, wordIndex + 1, board, word) ||  backtrack(r - 1, c, wordIndex + 1, board, word) ||    backtrack(r, c + 1, wordIndex + 1, board, word) ||    backtrack(r, c - 1, wordIndex + 1, board, word);
 
+        // Backtrack: restore original value
+        board[r][c] = temp;
 
-        boolean isWord = search(board, i+1,j,word,index+1) || 
-                        search(board, i-1,j,word,index+1) ||
-                        search(board, i,j+1,word,index+1) || 
-                        search(board, i,j-1,word,index+1);
-        board[i][j] = temp;
-        return isWord;
+        return found;
     }
 }
